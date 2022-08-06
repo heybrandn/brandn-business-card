5aea45602d12714bc8de766e1d04b00d86fce2a7
brandon <83041464+heybrandn@users.noreply.github.com>
2022-08-06 17:24:13 -0400

    Updated to include personal frontend portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+function open1() {
+	window.open("https://heybrandn.github.io/basketball-scoreboard/" , "_blank");
+}
+function open2() {
+	window.open("https://heybrandn.github.io/about-connecticut/" , "_blank");
+}
+function open3() {
+	window.open("https://heybrandn.github.io/passenger-counter/" , "_blank");
+}
+function open4() {
+	window.open("https://heybrandn.github.io/birthday-site/" , "_blank");
+}
+function open5() {
+	window.open("https://heybrandn.github.io/link-page/" , "_blank");
+}
+function open6() {
+	window.open("https://heybrandn.github.io/contact-site/" , "_blank");
+}
+function open7() {
+	window.open("https://heybrandn.github.io/basketball-scoreboard/" , "_blank");
+}
+function open8() {
+	window.open("https://heybrandn.github.io/basketball-scoreboard/" , "_blank");
+}
+
